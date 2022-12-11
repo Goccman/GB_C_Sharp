6a6153d225a27b2f3bc09ca35b3a272d39eac247
gocman <andreyrbn@gmail.com>
2022-12-12 03:48:02 +0600

    add task 1.8

---
+++ Homework_1/Task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Write("Введите число: ");
+int number = Int32.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"Четные числа от 1 до {number}: ");
+int num = 1;
+while(num <= number)
+{
+    if (num%2 == 0) Console.Write(num + " ");
+    num++;
+ 
+}
+Console.WriteLine();
