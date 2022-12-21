ec5edfb68dbeafd94617069405e29496289d4f35
gocman <andreyrbn@gmail.com>
2022-12-21 13:16:23 +0600

    add task_4

--- Homework_4/Task_3/Program.cs
+++ Homework_4/Task_3/Program.cs
@@ -5,3 +5,31 @@
 
 */
 
+
+int []InitArray(int deminsion){
+  int [] array = new int[deminsion];
+  Random rnd = new Random();
+
+  for (int i = 0; i < deminsion; i++)
+  {
+    array[i] = rnd.Next(0, 10);
+  }
+  return array;
+}
+
+
+void PrintArray(int [] array){
+  Console.Write("[");
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]}, ");
+    
+  }
+  Console.Write("]");
+  Console.WriteLine();
+
+}
+
+Console.Write("Элементы массива: ");
+int [] arr = InitArray(8);
+PrintArray(arr);
