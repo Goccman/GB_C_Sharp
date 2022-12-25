ef345e158ef8662c9b050ee9a86fabcc252de818
gocman <andreyrbn@gmail.com>
2022-12-26 00:41:00 +0600

    add Task_1

--- Homework_5/Task_1/Program.cs
+++ Homework_5/Task_1/Program.cs
@@ -8,14 +8,14 @@
 //инициализирует новый массив
 int[] InitArray(int length)
 {
-int[] array = new int[length];
-Random rnd = new Random();
-for (int i = 0; i < length; i++)
-{
-array[i] = rnd.Next(100,1000);
-}
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
 
-return array;
+    return array;
 }
 
 
@@ -23,31 +23,31 @@ return array;
 //печатает массив на экран
 void PrintArray(int[] array)
 {
-for (int i = 0; i < array.Length; i++)
-{
-Console.Write($"{array[i]} ");
-}
-Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
 
 
 //получает сумму четных элементов в массиве
 int GetEvenNumber(int[] arr)
 {
-int count = 0;
-for (int i = 0; i < arr.Length; i++)
-{
-if(arr[i]%2 == 0)
-count ++;
-}
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count++;
+    }
 
-return count;
+    return count;
 }
 
 
 Console.Write("Введите количество элементов в массиве: ");
 int numArray = Int32.Parse(Console.ReadLine() ?? "");
-int [] array = InitArray(numArray);
+int[] array = InitArray(numArray);
 PrintArray(array);
 int count = GetNumber(array);
 Console.WriteLine($"Количество четных чисел в массиве: {count}");
