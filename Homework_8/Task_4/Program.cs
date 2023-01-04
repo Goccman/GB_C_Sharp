d6ca5dc1ab89d2ffcebff80f416126cbed17cf54
gocman <andreyrbn@gmail.com>
2023-01-04 21:51:54 +0600

    add task_4

--- Homework_8/Task_4/Program.cs
+++ Homework_8/Task_4/Program.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
+Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
 которая будет построчно выводить массив, добавляя индексы каждого элемента.
 
 Массив размером 2 x 2 x 2
@@ -9,3 +9,95 @@
 26(1,0,1) 55(1,1,1)
 
 */
+
+
+
+
+//получить число с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+
+// задать массив и заполнить его числами
+void GetArray(int[,,] array3D)
+{
+    int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+    int number;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < array3D.GetLength(0); x++)
+    {
+        for (int y = 0; y < array3D.GetLength(1); y++)
+        {
+            for (int z = 0; z < array3D.GetLength(2); z++)
+            {
+                array3D[x, y, z] = temp[count];
+                count++;
+            }
+        }
+    }
+}
+
+
+
+// печать массива
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+  
+
+
+int x = GetNumber("Введите X: ");
+int y = GetNumber("Введите Y: ");
+int z = GetNumber("Введите Z: ");
+int[,,] array3D = new int[x, y, z];
+GetArray(array3D);
+Console.WriteLine();
+PrintArray(array3D);
+
+
